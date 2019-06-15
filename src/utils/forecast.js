fa0dc55ffa6efac7f04b5380d8aab9d12398d048
rimpu <aditibose1995@gmail.com>
2019-06-15 18:06:36 +0530

    change in forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree celsius out. High is '+ body.daily.data[0].temperatureHigh+' degree celsius and low is ' +body.daily.data[0].temperatureLow+ ' degree celsius. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
